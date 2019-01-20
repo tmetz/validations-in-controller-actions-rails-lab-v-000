9e3ce00ad32c867953ba578ead1d825de9078eef
Tammy Metz <tammy@tammymetz.com>
2019-01-20 18:28:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update!(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
