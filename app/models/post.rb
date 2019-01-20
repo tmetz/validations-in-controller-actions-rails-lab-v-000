70901671753dfb2d18f52abc8d568f1a74b49132
Tammy Metz <tammy@tammymetz.com>
2019-01-20 17:56:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction) }
+  validates :content, length: { minimum: 100 }
 end
